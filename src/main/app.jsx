130dc1aa603525818d32baef11358b03ce90a5ec
EDUARDO SIQUEIRA MONTEIRO DO CARMO <eduardo.carmo2@fatec.sp.gov.br>
2020-03-02 23:02:26 -0300

    primeiro commit

---
+++ src/main/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "modules/bootstrap/dist/css/bootstrap.min.css";
+import "modules/font-awesome/css/font-awesome.min.css";
+
+import Menu from "../template/menu";
+import Routes from "./routes";
+
+export default props => (
+  <div className="container">
+    <Menu />
+    <Routes />
+  </div>
+);
